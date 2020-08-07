c352d391e30c035176e604066d5b9e59635dbac2
ecardoso <everton.ap.cardoso@gmail.com>
2020-08-07 11:58:03 -0300

    Projeto de exemplo spring-boot

---
+++ src/main/java/br/com/springbootexercicio/SpringBootExercicioApplication.java
@@ -0,0 +1,13 @@
+package br.com.springbootexercicio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExercicioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExercicioApplication.class, args);
+	}
+
+}
